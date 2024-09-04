469fb8998b54235a0ce7ead1fd9950c4592e5928
Nabin khanal <khanalnabin310@gmail.com>
2024-09-04 08:31:58 +0545

    css

--- frontend/src/components/RelatedProducts/RelatedProduct.jsx
+++ frontend/src/components/RelatedProducts/RelatedProduct.jsx
@@ -15,7 +15,6 @@ function RelatedProduct() {
                         return <Item key={index} id={item.id} name={item.name} image={item.image} new_price={item.new_price} old_price={item.old_price} />
 
                     })
-
                 }
 
             </div>
